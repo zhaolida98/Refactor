51537a35b5afb3de2f36de05a1605f22dda2a7e0
zhaolida98 <11611803@mail.sustc.edu.cn>
2018-12-15 01:39:09 +0800

    Removing frequentRenterPoints

--- Customer.java
+++ Customer.java
@@ -19,28 +19,40 @@ class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
 
         for (Rental each : _rentals) {
             //determine amounts for each line
-            frequentRenterPoints += each.getFrequentRenterPoints();
 
             //show figures for this rental
             result.append("\t").append(each.getTitle());
             result.append("\t").append(String.valueOf(each.getCharge()));
             result.append("\n");
 
-            totalAmount += each.getCharge();				
         }
 
         //add footer lines
-        result.append("Amount owed is ").append(String.valueOf(totalAmount));
+        result.append("Amount owed is ").append(String.valueOf(getTotalCharge()));
         result.append("\n");
-        result.append("You earned ").append(String.valueOf(frequentRenterPoints));
+        result.append("You earned ").append(String.valueOf(getTotalFrequentRenterPoints()));
         result.append(" frequent renter points");
         return result.toString();
+    }
+
+    public int getTotalFrequentRenterPoints(){
+        int result = 0;
+        for (Rental each : _rentals) {
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
+    }
+
+    public double getTotalCharge() {
+        double result = 0;
+        for (Rental each : _rentals) {
+            result += each.getCharge();
+        }
+        return result;
     }
 
 }
